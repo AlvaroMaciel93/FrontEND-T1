7e3d08dde27754c332c0d560aa9a043beb103f57
Álvaro Maciel Cabral Dantas <142815803+AlvaroMaciel93@users.noreply.github.com>
2023-12-14 07:21:36 -0300

    Add files via upload

---
+++ FEB-005/script.ts
@@ -0,0 +1,46 @@
+// Importando jQuery
+import $ from jquery;
+
+// Definindo classes e objetos
+class Noticia {
+  titulo: string;
+  link: string;
+
+  constructor(titulo: string, link: string) {
+    this.titulo = titulo;
+    this.link = link;
+  }
+}
+
+function buscarNoticias() {
+  const urlNoticias = 'https://jokeapi.dev/joke/Any?type=single';
+  
+  $.ajax({
+    url: urlNoticias,
+    method: 'GET',
+    success: (data: any) => {
+      const noticia = new Noticia(data.joke, '');
+      exibirNoticia(noticia);
+    },
+    error: (error: any) => {
+      console.error('Erro ao buscar notícias:', error);
+    }
+  });
+}
+
+// Função para exibir notícias na página
+function exibirNoticia(noticia: Noticia) {
+  const noticiasDiv = $('.quadradonoticias'); // Div onde serão exibidas as notícias
+  
+  const noticiaHtml = `
+    <h3>NOTÍCIAS</h3>
+    <p>${noticia.titulo}</p>
+  `; // Estrutura HTML para exibir a notícia, você pode formatar conforme necessário
+  
+  noticiasDiv.html(noticiaHtml);
+}
+
+// Chamada da função para buscar e exibir notícias
+$(document).ready(() => {
+  buscarNoticias();
+});
